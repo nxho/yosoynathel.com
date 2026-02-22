86f529bdcad1d3817b33ecf4827a4da46c7e1a52
Nathaniel Ho <nxho93@gmail.com>
2026-02-22 18:00:47 -0500

    fix trailing slash

--- apps/photos/next.config.ts
+++ apps/photos/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  trailingSlash: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
